4b510d065391d1157561533c9ceddfb2c2421ee3
Mrwhite987 <Mrwhite987@hotmail.com>
2025-08-04 17:40:46 +0800

    Finish 06-11 exercises

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahTemp) {
+  const celTemp = (fahTemp - 32) * (5 / 9);
+  return roundToOneDecimal(celTemp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celTemp) {
+  const fahTemp = celTemp * (9 / 5) + 32;
+  return roundToOneDecimal(fahTemp);
 };
+
+const roundToOneDecimal = (num) => Math.round(num * 10) / 10;
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
