8dc23bb1614683dd3d9fcba53aca62760461c7fb
Mrwhite987 <Mrwhite987@hotmail.com>
2025-08-08 16:05:59 +0800

    Finish exercises 12-16

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, "");
+  const strArr = cleanedStr.split("");
+  const lowerStrArr = strArr.map((letter) => letter.toLowerCase());
+  const cleanedLowerStr = lowerStrArr.reduce((str, letter) => str + letter, "");
+  let reversedStr = "";
+  for (let i = 0; i < lowerStrArr.length; i++) {
+    const letter = lowerStrArr[lowerStrArr.length - i - 1];
+    reversedStr += letter;
+  }
+  return cleanedLowerStr == reversedStr;
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  people.forEach((person) => {
+    if (person.yearOfDeath === undefined) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+  });
+  people.sort(compareAge);
+  return people[0];
 };
+
+function compareAge(person1, person2) {
+  return (
+    person2.yearOfDeath -
+    person2.yearOfBirth -
+    (person1.yearOfDeath - person1.yearOfBirth)
+  );
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
